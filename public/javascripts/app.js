4f615741b08c96f76ac8f9162329f6110adec4b8
Kirsten <kirstenwedde@gmail.com>
2016-08-22 18:10:01 -0600

    added ui router and states

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,1 @@
-var app = angular.module('gardenApp', []); 
+var app = angular.module('gardenApp', ['ui.router']); 
---
+++ public/javascripts/routes.js
@@ -0,0 +1,55 @@
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+//$authProvider
+    $urlRouterProvider.otherwise("/");
+
+    $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl'
+    })
+    .state('product', {
+      url: '/our-product',
+      templateUrl: 'views/our-product.html',
+      controller: 'ProductCtrl'
+    })
+    .state('company', {
+      url: '/the-company',
+      templateUrl: 'views/the-company.html',
+      controller: 'CompanyCtrl',
+    })
+    .state('solve', {
+      url: '/we-solve',
+      templateUrl: 'views/we-solve.html',
+      controller: 'SolveCtrl',
+    })
+    .state('login', {
+    url: '/login',
+    templateUrl: 'views/login.html',
+    controller: 'LoginCtrl',
+    // resolve: { skipIfAuthenticated: skipIfAuthenticated }
+    })
+  .state('signup', {
+    url: '/signup',
+    templateUrl: 'views/signup.html',
+    controller: 'SignupCtrl',
+    // resolve: { skipIfAuthenticated: skipIfAuthenticated }
+    })
+    .state('account', {
+      url: '/account',
+      templateUrl: 'views/profile.html',
+      controller: 'ProfileCtrl',
+      // resolve: { loginRequired: loginRequired }
+    })
+    .state('dashboard', {
+      url: '/dashboard',
+      templateUrl: 'views/dashboard.html',
+      controller: 'DashboardCtrl',
+      // resolve: { loginRequired: loginRequired }
+    })
+    $locationProvider.html5Mode(true);
+
+
+    // $authProvider.loginUrl = '/login';
+    // $authProvider.signupUrl = '/signup';
+  })
