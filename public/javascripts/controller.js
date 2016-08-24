a1fe6de57c3fc6ae651cbb64e68c38939e29ffc6
Kirsten <kirstenwedde@gmail.com>
2016-08-24 16:31:31 -0600

    added johnny-five, fixed server api, and url hash issue

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -1,8 +1,8 @@
 app.controller('gardenCtrl', function($scope, $http){
    $scope.view = {};
 
-  //  $http.get('/api').then(function(plants){
-  //    console.log(plants);
-  //    $scope.view.plants = plants.data;
-  // })
+   $http.get('/api').then(function(plants){
+     console.log(plants);
+     $scope.view.plants = plants.data;
+  })
 });
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 var plants = require('../plant_data')
 
-// router.get('/api', function(req, res, next) {
-//   res.json(plants);
-// });
-//
+router.get('/api', function(req, res, next) {
+  console.log("api route accessed");
+  res.json(plants);
+});
+
 // exports.servePlants = function(req, res, next) {
 //   res.json(plants.data);
 // };
