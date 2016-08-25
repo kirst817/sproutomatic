31b57ea5d5ea84dca20cef5b8c8f407efd478432
Kirsten <kirstenwedde@gmail.com>
2016-08-25 10:10:38 -0600

    added basic arduino outline

--- arduino/index.js
+++ arduino/index.js
@@ -1,3 +1,16 @@
 //Arduino index.js
 
 var five = require("johnny-five");
+
+var board = new five.Board();
+board.on('ready', function(){
+  console.log("Board ready, arduino connected");
+  //sensor location(s)
+  //ie. led = new five.Led(13); 
+})
+
+//setInterval() for receiving data
+
+//function for first argument in setInterval
+
+console.log("\nWaiting for device to connect ....");
