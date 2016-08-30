0a7abe966468eedc28b6fb365eb07b3dfe3f694f
Kirsten <kirstenwedde@gmail.com>
2016-08-30 15:39:57 -0600

    added users route

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-// var users = require('./routes/users');
+var users = require('./routes/users');
 
 var app = express();
 
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public'))).listen(process.env.PORT 
 
 
 app.use('/', routes);
-// app.use('/users', users);
+app.use('/users', users);
 
 app.use('*', function(req, res) {
   res.redirect('/#' + req.originalUrl);
