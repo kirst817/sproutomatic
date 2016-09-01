8df60f6fe7240a5d5e7f2476daa8f63959a44387
Kirsten <kirstenwedde@gmail.com>
2016-09-01 14:22:48 -0600

    added start of knex to add api data to db

--- routes/index.js
+++ routes/index.js
@@ -11,20 +11,61 @@ var plants = require('../data/plant_data');
 var growdata = require('../data/dummy_grow_data');
 
 //plant grow maintenence levels
-router.get('/api', function(req, res, next) {
-  console.log("api route accessed");
-  //gets the plant js file and returns as json file
 
-  knex('plants')
-  res.json(plants);
+function plantData() {
+    return knex('plantData');
+}
+
+function plants() {
+    return knex('plants');
+}
+
+router.get('/api', function(req, res, next) {
+    console.log("api route accessed");
+    //gets the plant js file and returns as json file
+
+    // plants()
+    res.json(plants);
 });
 
 //dummy grow data
+//original ------
 router.get('/api2', function(req, res, next) {
   console.log("api2 route accessed");
-
   res.json(growdata);
 });
+//-------original ------
+
+// router.get('/api2', function(req, res, next) {
+//     console.log("api2 route accessed");
+//     plantData()
+//         .where('logged_at', date)
+//         .first()
+//         .then(function(peak) {
+//             if (growdata) {
+//                 res.json(growdata)
+//             } else {
+//                 request('https://data.sparkfun.com/output/NJXGOwpYV3SmAoGZMvWa.json', function(error, response, body) {
+//                     if (!error && response.statusCode == 200) {
+//                         var growdata = {};
+//                         // growdata.logged_at = date;
+//                         // growdata.sunlight = sunlight;
+//                         growdata.moisture = analogmoisture;
+//                         growdata.temperature = temperature;
+//                         growdata.humidity = humidity;
+//                         for (var i = 0; i < growdata.length; i++) {
+//                             plantData()
+//                               .insert(growdata)
+//                               .then(function() {
+//                                     res.json(growdata);
+//                                 })
+//                         }
+//                     }
+//                 })
+//             }
+//         })
+// });
+
 
 //PLANT DATA from sparkfun
 //uncomment when hooked up!!!
