ac48c2a1b81705d0885fbe7123a9181f2579acf3
Kirsten <kirstenwedde@gmail.com>
2016-09-07 22:39:16 -0600

    styling and server changes

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public'))).listen(process.env.PORT || 3000);
+app.use(express.static(path.join(__dirname, 'public'))).listen(process.env.PORT || 8080);
 
 
 app.use('/', routes);
